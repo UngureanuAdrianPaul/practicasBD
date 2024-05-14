f9967472ecc9ec65e2c6192b1796e93471021d33
UngureanuAdrianPaul <adrianpaul.s371152@cesurformnacion.com>
2024-05-14 11:53:40 +0200

    codigo segun apuntes

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    
+        CreateTableBank miTable = new CreateTableBank();
+        miTable.initialize();
+       
+    }
+    
+}
